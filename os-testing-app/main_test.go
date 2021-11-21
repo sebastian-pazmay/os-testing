87dceb21e344793ea932cbf98a943478ddf37769
Sebastian Pazmay <spazmay90@gmail.com>
2021-11-21 21:56:08 +0100

    include debugging commands

--- os-testing-app/main_test.go
+++ os-testing-app/main_test.go
@@ -9,13 +9,13 @@ import (
 var expectedVersion = "Ubuntu 18.04.5"
 
 func TestSuite1(t *testing.T) {
-	fmt.Println("This is the testing function")
+	fmt.Println("This is the testing suite 1")
 	fmt.Println(testSuites.VerifyOSVersion(expectedVersion))
 	fmt.Println(testSuites.VerifyRootPasswd())
 }
 
 func TestSuite2(t *testing.T) {
-	fmt.Println("This is the testing function")
+	fmt.Println("This is the testing suite 2")
 	testSuites.VerifyOSVersion(expectedVersion)
 	testSuites.VerifyRootPasswd()
 }
